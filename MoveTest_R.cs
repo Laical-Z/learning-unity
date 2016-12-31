379954bdeaadfd187e09347000f954e087d5a253
wuhuarou <luoyouming@hotmail.com>
2016-12-31 23:01:42 +0800

    Collider

--- MoveTest_R.cs
+++ MoveTest_R.cs
@@ -36,5 +36,13 @@ public class MoveTest_R : MonoBehaviour {
         if (Input.GetKey(KeyCode.D)){
             cube_R.MovePosition(cube_T.position + Vector3.right * 0.1f);
         }
+        //如果一个物体添加了Rigidbody组件，那么必须也要添加一个Collider组件。因为物体与物体之间碰撞是物体的Collider组件的碰撞，所以如果只添加了Rigidbody而不添加Collider，是没有意义的。
+        //unity的基本模型(cube之类的)，当你创建的时候，一般是默认带有Colider组件的。
+
+        //常见的基础Collider形式有：
+        //Box Collider(Center：中心位置；Size：范围大小)、
+        //Sphere Collider(Center：中心位置；Radius：半径大小)、
+        //Capsule Collider(Center：中心位置；Radius：半径大小；Height：高度；Direction：Height属性的伸缩轴向)、
+        //Mesh Collider(这是一个网格状的“薄膜”，用来覆盖一些复杂的物体。Meth：根据你所选择的基本网格形状来构建整个“薄膜”)。
     }
 }
